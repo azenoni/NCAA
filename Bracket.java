85e4b1fbf170a7e06a964cbe63d9e6653fefea90
apcs <apcs@mvhs-u528ap06.dcsdk12.org>
2014-04-01 09:06:13 -0600

    not work

--- Bracket.java
+++ Bracket.java
@@ -3,13 +3,28 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Bracket {
-	private ArrayList<Region> region; 
+	private ArrayList<Region> regions = new ArrayList<Region>(); 
+	private ArrayList<Team> temp = new ArrayList<Team>();
+	public void main(String[] args) {
+		System.out.println(print(regions));
+	}
+
+	public String print(ArrayList<Region> regionz){
+		for (int i = 0; i<regionz.size(); i++) {			
+			for (int z = 0; z<regionz.get(i).get().size(); z++) {
+					regionz.get(i).get().get(z).name();								
+			}
+		}
+	}
 	public Bracket(ArrayList<Team> teams) {
 		ArrayList<Team> temp = new ArrayList<Team>();
+
 		for (int i = 0, z = 0; i <= 16; i++) {			
-			temp.add(teams.get(i));			
+			temp.add(teams.get(i));
+
 			if (i==16 && z<5) {
-				region = new ArrayList<Region>(temp);
+				Region region = new Region(temp);	
+				regions.add(region);
 				temp = new ArrayList<Team>();
 				z++;
 				i=0;
--- Region.java
+++ Region.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 public class Region {
 	private ArrayList<Team> teams = new ArrayList<Team>();
 	public Region(ArrayList<Team> teams) {
-
+		this.teams = teams;
+	}
+	public ArrayList<Team> get(){
+		return teams;
 	}
 }
